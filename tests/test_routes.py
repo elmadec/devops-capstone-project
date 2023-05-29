cf268154d106dc9c10d229534c59dd092ee8dc2f
dantawalli <buharinasir1996@gmail.com>
2023-05-29 14:40:55 -0400

    Added REST API and Tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,7 +138,7 @@ class TestAccountService(TestCase):
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
     
-     def test_get_account_list(self):
+    def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
         resp = self.client.get(BASE_URL)
@@ -168,6 +168,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_method_not_allowed(self):
-         """It should not allow an illegal method call"""
+        """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
